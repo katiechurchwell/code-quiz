92c2aee15d0771a222214d20680afe79b51d6379
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2021-11-28 20:59:12 -0800

    initial question answer

--- assets/script.js
+++ assets/script.js
@@ -8,26 +8,45 @@ var StartQuiz = function () {
 startBtn.addEventListener("click", StartQuiz);
 
 // THEN a timer starts and I am presented with a question
-var counter = 60;
+var counter = 10;
 
 var countdown = function () {
-  document.getElementById("timer-container").innerHTML = counter;
+  document.getElementById("timer-container").innerHTML = "Timer: " + counter;
   counter--;
-  if (counter === 0) {
-    console.log("blastoff");
-    clearInterval(startCountdown); //stops when 0
+  if (counter === -1) {
+    clearInterval(startCountdown);
   }
 };
 
 var startCountdown = setInterval(countdown, 1000); //milliseconds
 
+//Questions
+const question1 = {
+  q: "Question",
+  a: "Answer",
+};
+
 // WHEN I answer a question
 var presentQuestion = function () {
+  //remove start button
+  var startBtnContainer = document.getElementById("start-button-container");
+  startBtnContainer.remove();
+  startBtn.remove();
+
   //create div
-  //add text content from array?
-  //create buttons for multiple choice
-  //if else code for correct button
+  var questionContainer = document.createElement("div");
+  questionContainer.setAttribute("class", "question");
+  document.body.appendChild(questionContainer);
+  //display question
+  questionContainer.innerHTML = question1.q;
+//create buttons for multiple choice
+var answerContainer = document.createElement("button");
+answerContainer.setAttribute("class", "answer");
+document.body.appendChild(answerContainer);
+answerContainer.innerHTML = question1.a;
+//if else code for correct button
 };
+presentQuestion();
 
 // THEN I am presented with another question
 
