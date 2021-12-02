a4848fa0805adab3c0aea5a5b2bb888e55186dfd
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2021-12-01 19:58:12 -0800

    if highscore name null, nothing happens

--- assets/highscore.js
+++ assets/highscore.js
@@ -17,9 +17,13 @@ var scores = [
 
 //make object with userInitials as name and userScore as score
 window.onload = function recordScore() {
+if (userInitials == null){
+  //nothing
+}
+else {
 let user = { name: userInitials, score: userScore };
 scores.push(user);
-
+};
 //sort array by score
 scores.sort(function (a, b) {
   return b.score - a.score;
